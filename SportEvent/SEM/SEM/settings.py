6d93ce93206d0810f2e515d28f22940e508484e5
Avinash-006 <140252855+Avinash-006@users.noreply.github.com>
2024-09-23 12:03:54 +0530

    Update settings.py

--- SportEvent/SEM/SEM/settings.py
+++ SportEvent/SEM/SEM/settings.py
@@ -114,7 +114,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY', 'Your API key')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
